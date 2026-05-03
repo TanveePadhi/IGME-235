016b2a60f50a357c280036d6c16ee32ab792b0ca
TanveePadhi <tp5262@rit.edu>
2026-05-02 21:41:48 -0400

    added button on proposal and documentation

--- project3/js/buttons.js
+++ project3/js/buttons.js
@@ -4,7 +4,6 @@ document.getElementById("documentation").onclick = function () {
     window.open("documentation.html", "_blank");
 };
 
-
 document.getElementById("proposal").onclick = function () {
     window.open("proposal.html", "_blank");
 };
